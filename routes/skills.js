e0431768aa4a0f812cbd81ad18115f345778a499
kamsinghGA <kamaljitga@gmail.com>
2023-06-15 23:41:43 -0400

    added show and index functionality, fixed errors

--- routes/skills.js
+++ routes/skills.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 var skillsCtrl = require('../controllers/skills')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', skillsCtrl.index);
+router.get('/:id', skillsCtrl.show);
 
 module.exports = router;
